d1318712be473fa2a513ee5f617e376c8f043976
raushan1156 <iamraushankumar56@gmail.com>
2025-12-13 19:19:57 +0530

    formLogin()
    Understood the formLogin working
    Altered its default flow and tested
    explored the csrf and SessionCreationPolicy enum

---
+++ employee-management/src/main/java/com/miniprojects/Employee/employee_management/config/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.miniprojects.Employee.employee_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api").hasAnyRole("MANAGER")
+                        .requestMatchers("/api/**").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .sessionManagement(sessionConfig -> sessionConfig.
+                        sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//                .formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    UserDetailsService inMemoryUserDetailsService(){
+        UserDetails managerUser = User
+                .withUsername("raushan")
+                .password(passwordEncoder().encode("raushan123"))
+                .roles("MANAGER")
+                .build();
+
+        UserDetails adminUser = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("ADMIN"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(managerUser, adminUser);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
