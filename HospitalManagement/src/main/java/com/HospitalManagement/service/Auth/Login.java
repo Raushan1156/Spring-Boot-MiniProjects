ecd62cfb80be898f358e6dd4c94a5b0f484208e8
raushan1156 <iamraushankumar56@gmail.com>
2025-12-17 13:13:41 +0530

    Add exception for Bad Credential
    Just need to add the try-catch and catch the exception with printing a message and error trace to debug easily

--- HospitalManagement/src/main/java/com/HospitalManagement/service/Auth/Login.java
+++ HospitalManagement/src/main/java/com/HospitalManagement/service/Auth/Login.java
@@ -1,17 +1,11 @@
 package com.HospitalManagement.service.Auth;
 
 import com.HospitalManagement.dto.LoginDto;
-import com.HospitalManagement.dto.SignUpDto;
-import com.HospitalManagement.dto.UserDto;
 import com.HospitalManagement.entity.Users;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -23,19 +17,26 @@ public class Login {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
 
-    public String login(LoginDto loginDto){
-        System.out.println("inside login method of auth service...");
-        // need a authManager and call authenticate() method ->
-        // -> Here our purpose is to take the principal id to generate the token
-        Authentication authenticate = authenticationManager
-                .authenticate(
-                        new UsernamePasswordAuthenticationToken(
-                                loginDto.getUsername(),
-                                loginDto.getPassword()
-                        )
-                );
-        Users userLoginDetails = (Users) authenticate.getPrincipal();
-        System.out.println("Details are:\n"+userLoginDetails);
-        return jwtService.generateToken(userLoginDetails);
+    public String login(LoginDto loginDto) {
+
+        try {
+            System.out.println("inside login method of auth service...");
+            // need a authManager and call authenticate() method ->
+            // -> Here our purpose is to take the principal id to generate the token
+            Authentication authenticate = authenticationManager
+                    .authenticate(
+                            new UsernamePasswordAuthenticationToken(
+                                    loginDto.getUsername(),
+                                    loginDto.getPassword()
+                            )
+                    );
+            Users userLoginDetails = (Users) authenticate.getPrincipal();
+            System.out.println("Details are:\n" + userLoginDetails);
+            return jwtService.generateToken(userLoginDetails);
+        } catch (Exception e) {
+            System.out.println("Authentication failed");
+            e.printStackTrace();
+            throw e; // rethrow
+        }
     }
 }
