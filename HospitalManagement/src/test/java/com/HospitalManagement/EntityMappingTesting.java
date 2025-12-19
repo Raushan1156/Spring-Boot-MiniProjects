4bc06c74041332e86034586cf20682def2cb694b
raushan1156 <iamraushankumar56@gmail.com>
2025-12-19 18:42:38 +0530

    Transaction | RollBack(false) | Commit
    Understood the default behavior of transactional
    Auto-rollback happens in spring test but not in production
    used commit or rollback to see the changes in db via test class.

--- HospitalManagement/src/test/java/com/HospitalManagement/EntityMappingTesting.java
+++ HospitalManagement/src/test/java/com/HospitalManagement/EntityMappingTesting.java
@@ -4,13 +4,21 @@ import com.HospitalManagement.entity.InsuranceEntity;
 import com.HospitalManagement.entity.PatientEntity;
 import com.HospitalManagement.repository.InsuranceRepository;
 import com.HospitalManagement.repository.PatientRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 
 import java.util.Collections;
 
+@Transactional
+//@Rollback(false)
+@Commit
 @SpringBootTest
 public class EntityMappingTesting {
 
@@ -19,6 +27,8 @@ public class EntityMappingTesting {
     @Autowired
     private InsuranceRepository insuranceRepository;
     private InsuranceEntity insuranceEntity;
+    @PersistenceContext
+    private EntityManager entityManager;
 
 
     @Test
@@ -54,5 +64,7 @@ public class EntityMappingTesting {
         Hibernate: select pe1_0.id,pe1_0.birth_date,pe1_0.blood_group,pe1_0.created_date,pe1_0.email,pe1_0.gender,ie1_0.id,ie1_0.created_at,ie1_0.policy_number,ie1_0.provider,ie1_0.valid_until,pe1_0.name from patient pe1_0 left join insurance ie1_0 on pe1_0.id=ie1_0.patient_id where pe1_0.id=?
         Hibernate: delete from insurance where id=?
          */
+
+        entityManager.flush();
     }
 }
